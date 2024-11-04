ba3a4384817e9cc915617e73acdc05561001b99a
Noman <nomanarifk@gmail.com>
2024-11-05 03:15:08 +0500

    Add constants

---
+++ Common/Constants.cs
@@ -0,0 +1,9 @@
+namespace MIRSAL.Common
+{
+    public static class RequestStatus
+    {
+        public const string Pending = "Pending";
+        public const string InProgress = "In Progress";
+        public const string Completed = "Completed";
+    }
+}
